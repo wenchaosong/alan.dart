fc1a953b927a569213f8f0ce144a4de1526e9267
songwenchao <songwenchao@statefortune.com>
2023-03-30 02:58:36 +0800

    组装 json

--- example/example.dart
+++ example/example.dart
@@ -2,63 +2,46 @@ import 'dart:typed_data';
 
 import 'package:alan/alan.dart';
 import 'package:alan/proto/cosmos/bank/v1beta1/export.dart' as bank;
-import 'package:grpc/grpc.dart';
 import 'package:hex/hex.dart';
 
 Future main() async {
-  // Create a wallet
-  final networkInfo = NetworkInfo(
-    bech32Hrp: 'scc',
-    grpcInfo: GRPCInfo(
-      host: 'http://yuangao.cuxing.tech',
-      port: 1317,
-      credentials: ChannelCredentials.secure(),
-    ),
-    lcdInfo: LCDInfo(
-      host: 'http://yuangao.cuxing.tech',
-      port: 26657,
-    ),
-  );
-
-  final privateKey =
+  final privateKey1 =
       '4f78924b4b91b171bf45ec4efc9c30cc7013fc89c17902fb0fd885b351b62bea';
-  final wallet =
-      Wallet.import(networkInfo, Uint8List.fromList(HEX.decode(privateKey)));
+  final privateKey2 =
+      '6cabf5f6f044045120da63d6ea7ea9b8a2971cbda7df76412478ee4343e04e06';
+  final address1 = 'scc1ka445hq9z0ucuwc2ej0vcc2uuvljxxqkyalesv'; //7076 7142
+  final address2 = 'scc1w9mqg9h0eparfzuhr46wq0s4k0u5l8k56k5p3z'; //2066 1934
 
-  final message = bank.MsgSend.create()
-    ..fromAddress = wallet.bech32Address
-    ..toAddress = 'scc1w9mqg9h0eparfzuhr46wq0s4k0u5l8k56k5p3z';
-  message.amount.add(
-    Coin.create()
-      ..denom = 'scu'
-      ..amount = '33',
-  );
+  var hash = await sign(privateKey2, address2, address1, '50');
+  print('hash: $hash');
+}
 
+Future<String> sign(
+    String priKey, String fromAdd, String toAdd, String amount) async {
   var net = NetworkInfo.fromSingleHost(
-    bech32Hrp: wallet.bech32Address,
+    bech32Hrp: 'scc',
     host: 'yuangao.cuxing.tech',
   );
+  final wallet = Wallet.import(net, Uint8List.fromList(HEX.decode(priKey)));
+
+  final message = bank.MsgSend.fromJson('{"1":"$fromAdd","2":"$toAdd","3":[{"1":"scu","2":"$amount"}]}');
+  //final message = bank.MsgSend.fromJson('{"1":"scc1ka445hq9z0ucuwc2ej0vcc2uuvljxxqkyalesv","2":"0","3":"297","4":"","5":"2023-03-30 01:32:03","6":"2023-03-30 01:32:09","7":"297_1680111123","8":"1626","9":"测试签名1","10":"1","11":"0","12":"11","13":"12","14":"8","15":"0","16":"0","17":"0","18":"2023-03-30 01:32:03","19":"2023-03-30 01:32:09","20":"175","21":"16378302178273566720001","22":"1637830217827356672","23":"181****6884","24":"QmT1eFvEpurkFRc3EmVMwbfGJH4Uu628HdauN5ySJc41t9"}');
 
   final signer = TxSigner.fromNetworkInfo(net);
   final fee = Fee()
     ..amount.add(Coin(
-      amount: '102',
       denom: 'scu',
+      amount: amount,
     ));
   fee.gasLimit = 200000.toInt64();
 
-  final tx = await signer.createAndSign(
-    wallet,
-    [message],
-    fee: fee,
-  );
+  final tx = await signer.createAndSign(wallet, [message], fee: fee);
   final txSender = TxSender.fromNetworkInfo(net);
-  final response = await txSender.broadcastTx(tx);
+  var response = await txSender.broadcastTx(tx);
 
-  // Check the result
   if (response.isSuccessful) {
-    print('Tx sent successfully. Response: ${response}');
+    return response.txhash;
   } else {
-    print('Tx errored: ${response}');
+    return '';
   }
 }
